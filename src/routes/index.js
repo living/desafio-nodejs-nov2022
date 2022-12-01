3ec2522a7a44107965a215a4997bc70dec3d29d5
henrique <henrique..florencio307@gmail.com>
2022-12-01 13:21:12 -0300

    adicionando compatibilidade com swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,12 @@ function appConfig(app) {
         express.json(),
         livrosRouter,
         editorasRouter,
-        admRouter
+        admRouter,
+        function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "https://desafio-living-nodejs.herokuapp.com/livros/"); // update to match the domain you will make the request from
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        },
     )
 }
 // TEMPLATES DE REQUISIÇÃO PARA LIVROS E EDITORA RESPECTIVAMENTE
